b97c14859084ea64bd32e28d97c090b188e9a14f
Andrei Piliushenko <pionbox@gmail.com>
2021-11-29 01:03:31 +0300

    fix task 1, args1 fromFolder, args2 destFolder

--- index.js
+++ index.js
@@ -1,54 +1,47 @@
 const path = require('path')
-const { readdir, stat, link, access, constants, mkdir } = require('fs')
 const fs = require('fs')
 
 const config = {
-  entry: path.resolve(__dirname, "sourceFolder"),
-  dest: path.resolve(__dirname, "destFolder"),
+  entry: process.argv[2] ? path.resolve(__dirname, process.argv[2]) : path.resolve(__dirname, "sourceFolder"),
+  dest: process.argv[3] ? path.resolve(__dirname, process.argv[3]) : path.resolve(__dirname, "destFolder"),
 }
 
 function reader(src) {
   fs.readdir(src, function (err, files) {
     if (err) throw err
 
-    if (!files.length) throw new Error('нет файлов!')
-
-    files.forEach(function (file) {
+    files.forEach(function (file, ind, array) {
       const currentPath = path.resolve(src, file)
       fs.stat(currentPath, function (err, stats) {
         if (err) throw err
 
         if (stats.isDirectory()) {
-          reader(currentPath)
+            reader(currentPath)
         } else {
           console.log('file', currentPath)
-          pathDestDir = path.resolve(config.dest, path.basename(currentPath)[0].toUpperCase())
+          const pathDestDir = path.resolve(config.dest, path.basename(currentPath)[0].toUpperCase())
           console.log('destFolder', pathDestDir);
           fs.stat(pathDestDir, function (err) {
             if (!err) {
-              console.log("существует");
               newPath = path.resolve(pathDestDir, path.basename(currentPath))
               fs.link(currentPath, newPath, function (err) {
                 if (err) {
                   throw err;
                 }
-                console.log("скопрована после существования");
+
               });
             }
             else if (err.code === 'ENOENT') {
-              console.log("не существует");
               fs.mkdir(pathDestDir, err => {
                 if (err) throw err; // не удалось создать папку
-                console.log('Папка успешно создана');
                 newPath = path.resolve(pathDestDir, path.basename(currentPath))
                 fs.link(currentPath, newPath, function (err) {
                   if (err) {
                     throw err;
                   }
-                  console.log("скопрована после создания");
                 });
               });
-            }
+            } else throw err
           });
         }
       })
@@ -57,7 +50,25 @@ function reader(src) {
 }
 
 try {
-  reader(config.entry)
+  fs.stat(config.entry, function (err) {
+    if (!err) {
+      fs.stat(config.dest, function (err) {
+        if (!err) {
+          reader(config.entry)
+        } else if (err.code === 'ENOENT') {
+          fs.mkdir(config.dest, function (err) {
+            if (!err) {
+              reader(config.entry, 0)
+            } else {
+              console.log("ошибка при создания директории ", config.dest);
+            }
+          })
+        }
+      })
+    } else {
+      console.log("нет исходной директории ", config.entry);
+    }
+  })
 } catch (error) {
   console.log(error)
 }
